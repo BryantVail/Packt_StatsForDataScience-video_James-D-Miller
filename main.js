219b811f760a45d497ed7acfd56454feaf4a677b
bxvail <BryantVail@gmail.com>
2018-05-06 15:54:03 -0400

    oing

--- main.js
+++ main.js
@@ -10,7 +10,31 @@ Statistical Population:
     intension
         represent total population for a particular area of interest
 
+Probability:
+    compares to:
+        statistical thought process
+    Analyzing frequency of past events
+    Observation evaluation & explanation
+    Associate various individual events
+    Events relating to one another:
+        governs the rules that will need to be followed when creating methods
+False Positives:
+    Statistical concept:
+        **Very Important
+    Mistake || errored result
+    "False Alarm"
+    Understood by:
+        "Considering idea of recordset || statistical population"
 
+    Statistical Inference:
+        usedTo:
+            create sample data
+        Process of:
+            deducing options of an underlying distribution analysis
+        sometimes called:
+            Inferential statistical analysis
+        Testing various hypotheses & deriving estimates
+            **Yes! the interesting part of data science || the reason I got interested I should say
 */
 
 
