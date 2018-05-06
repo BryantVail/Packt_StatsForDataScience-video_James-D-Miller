9d50eac3ca5ad8ee4cbe6329457e4e8a29836c76
bxvail <BryantVail@gmail.com>
2018-05-06 14:07:27 -0400

    init

--- main.js
+++ main.js
@@ -1,48 +1,69 @@
-//video 1-2_KeyObjectivesOfDataScience
+//video 1-3_commonTerminologies
 
 /*
 
 data science: a progression or evolution of thought or steps
 
-collecting
-    inference requires data
-    ample sources
-    methods constinue to grow
-    perspective NEW to a data developer
-    supplementation
-        graphics
 
-processing
-    skills
-    aspects of processing
-        formatting
-            setting data types
-            aggregating values
-            reordering
-            dropping columns
-        cleansing
-            default or missing values;
-            incomplete or inapposite values
-            profiling
-                context: statistical context
-exploring/visualizing
-    "dig deeper" 
-        validating assumptions or findings
-    graphic representations
-analyzing
-    attempt to shape a story based on an observation
-    slice & dice:
-        analytics or BI packages
-            create concrete data storyline
-    may double back to previous phase
-    determine data is where analysis can begin
-deciding based on insight
-    value gathering from insight
-    used as input to make better decisions
-    recommendations & shares data behind insights
 
-(very fluid process)
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
