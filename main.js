99f3989b4dc8ce27b988c63f794803f8ee7abafc
bxvail <BryantVail@gmail.com>
2018-05-06 17:43:38 -0400

    end

--- main.js
+++ main.js
@@ -11,20 +11,92 @@ this video:
     resolving common data issues,
     cleaning techniques
 
-
-     
-
-
-
-
-
-
-
-
-
-
-
 */
+var dataScience = dataScience || {};
+dataScience = {
+    "dataCleaning":{
+        "aka":[
+            "data scrubbing",
+        ],
+        "involves":[
+            "detecting and addressing errors",
+            "omissions",
+            "inconsistencies within data",
+        ],
+        "process":[
+            "use logic to examine and evaluate your data pool",
+            "quality can be affected by thge way data is entered, stored, and managed",
+            "involved correcting, replacing, removing data points",
+        ],
+        "DataDeveloper":[
+            "must know the importance of improving level of data quality",
+            "addressing data quality requires routine",
+            "regular review and evaluation of data",
+        ],
+        "dataIssues":[
+            "accuracy",
+            "completeness",
+            "relevance",
+            "update status",
+            "consistency",
+            "appropriateness",
+            "reliability",
+            "accessibility",
+        ],
+        "cleaningTechniques":[
+            {
+                "value":"identify data points",
+                "types":[
+                    "outliers",
+                    "unfollow pattern within data",
+                ],
+            },
+            {
+                "value":"validity checking",
+                "description":
+                    "applying rules to data to determine if its valid or not",
+
+            },//end validity checking
+            {
+                "value":
+                    "enhancement",
+                "description":
+                    "completing data by adding related information",
+                
+            },//end enhancement
+            {
+                "value":
+                    "harmonization",
+                "description":
+                    "data values are converted, or mapped to other, more desirable values",
+            },//end harmonization
+            {
+                "value":
+                    "standardization",
+                "description":
+                    "changing a reference dataset to a new standard; standard codes.",
+            },
+            {
+                "value":"domain expertise",
+                "description":
+                    "removing || modifying data values in data file",
+            },
+        ],//end cleaningTechniques
+    },//end dataCleaning
+
+    "R Programming Language":{
+        "notes":[
+            "well fitted for data science",
+            "works with complex datasets",
+            "allows handling of data through modeling functions"
+        ],
+    },
+
+
+
+}//end dataScience
+
+
 
 
 
