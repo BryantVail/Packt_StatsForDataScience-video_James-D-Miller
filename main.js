d43fadd526c783b7880aaab9ad2d6b394f994872
bxvail <BryantVail@gmail.com>
2018-05-06 16:26:06 -0400

    ing

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 /*
 
-Statistical Population:
+"Statistical Population":
     "recordset" or set of "records"
     similar items or events
     existing || hypothetical objects
@@ -26,16 +26,175 @@ False Positives:
     Understood by:
         "Considering idea of recordset || statistical population"
 
-    Statistical Inference:
-        usedTo:
-            create sample data
-        Process of:
-            deducing options of an underlying distribution analysis
-        sometimes called:
-            Inferential statistical analysis
-        Testing various hypotheses & deriving estimates
-            **Yes! the interesting part of data science || the reason I got interested I should say
+Statistical Inference:
+    usedTo:
+        create sample data
+    Process of:
+        deducing options of an underlying distribution analysis
+    sometimes called:
+        Inferential statistical analysis
+    Testing various hypotheses & deriving estimates
+        **Yes! the interesting part of data science || the reason I got interested I should say
+
+Regression:
+    Used for:
+        determining relationships amoung variables
+    Understanding of:
+        what a variable is
+    VariableTypes:
+        Dependent
+        Independent
+
+Fitting:
+    how well a statistical describes observations
+    Descrepancy detection attempt 
+    Observed values & probable values
+    Probable Values:
+        aka: distribution || probablility distribution
+    data scientist use:
+        Fits a probability distribution to a series of data
+
+"Categorical data":
+    "use":
+        "associate meaning with data",
+    "categorized values or numerals",
+    "sub group of data, sorted into a category or multiple categories",
+    "sub-populations",
+
+"classification":
+    "identify which catgegory should be grouped",
+    "Process Name":
+        "classifier"
+
+"clustering":
+    "divide data occurrences into homogeneous subsets",
+    "Do not":
+        "use a predeternined set of groups as in classification",
+        "Common for":
+            "data mining that is exploratory",
+            "statistical data analysis",
+
+"Statistical Comparison":{ 
+    "process of":
+        "data scientists performing analysis",
+    "viewing similariti9es or variances of 2 or more groups",
+    "comparing populations"
+},
+
+"Coding":{
+    "processUsedTo":
+        "preparing datra for analysis",
+    "where":
+        "both quantitative && qualitative data are categorized"
+    "performed for":[
+        "more effective for running statistical models",
+        "computers understand the variables",
+        "accountability",
+    ]
+},
+
+"distributions":{
+    "notes":[
+        "visualization showing all the possible values",
+        "attempts to show percentatge of individuals in each group"
+    ],
+},
+
+"dataMining":{
+    "aka":[
+        "knowledge discovery"
+    ],
+    "basedOn":
+        "processing through or analyzing data",
+    "softwareModel":
+        "dedicated of just several analytical approaches",
+    "value":
+        "finding correlations or patterns",
+
+},
+
+"DecisionTrees":{
+    "type":
+        "visualization"
+    "value":
+        "a diagram that looks like a tree",
+    "usedTo":
+        [//represent
+            "optional decision paths",
+            "predicted outcome for each path selected",
+        ],
+    "notes":
+        [
+            "common way to display logic of data science process",
+        ]
+},
+
+"MachineLearning":{
+    "notes":[
+        "most intriguing and exciting areas of data science",
+
+    ],
+    "value":
+        "Method of training a computer to make or improve predictions",
+    "types":[
+        "supervised learning",
+        "unsupervised learning",
+    ],
+},
+
+"visualization":{
+    "value":
+        "technique for creating a graphic to communicate a message", 
+    "notes":[
+        "make something complex appear simple",
+        "used in almost every stgep in the data science process",
+    ]
+}
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
